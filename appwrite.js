55deb5dfb3c6fa6db8de047964f60c3d938f16d6
Tsvetomir Mutashki <95773359+Tsvetomir18227@users.noreply.github.com>
2024-06-13 14:36:35 +0300

    Add files via upload

---
+++ appwrite.js
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    platform: 'com.mutashki.nutrain',
+    projectId: '6666f636003ba4b3e317',
+    databaseId: '6666f7fb00142e0b6773',
+    userCollectionId: '6666f8220011a3d011e1',
+    videosCollectionId: '6666f849000e900c30f4',
+    storageId: '6666f9f8002f20aee871'
+}
